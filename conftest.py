2d7a9dbb659e695e2c09ee46854998b51bebce09
iDreamlike <bersenev@mail.ru>
2019-12-04 00:30:20 +0300

    Урок 6.2, шаг 6: Все доделал

---
+++ conftest.py
@@ -0,0 +1,21 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
+
+def pytest_addoption(parser):
+    parser.addoption('--language', action='store', default='ru',
+                     help="Choose language: ...")
+
+
+@pytest.fixture(scope="function")
+def browser(request):
+    language = request.config.getoption("language")
+    options = Options()
+    options.add_experimental_option('prefs', {'intl.accept_languages': language})
+    link = "http://selenium1py.pythonanywhere.com"
+    browser = webdriver.Chrome(options=options)
+    browser.get(link)
+    yield browser
+    print("\nquit browser..")
+    browser.quit()
