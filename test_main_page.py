f189f02f0e2307a663bdf686b85628a4fc825eb8
Сергей <bersenev@mail.ru>
2019-12-04 15:44:32 +0300

    урок 6.3 шаг 7. Начал

--- test_main_page.py
+++ test_main_page.py
@@ -1,8 +1,10 @@
 from .pages.main_page import MainPage
+from .pages.locators import MainPageLocators
 
 
 def test_guest_can_go_to_login_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/"
+    link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209?promo=midsummer"
+    # link = "http://selenium1py.pythonanywhere.com/"
     page = MainPage(browser, link)
     page.open()
     page.go_to_login_page()
