1acdc1f4d247b42605bd65cc53a2006d87c8842c
iDreamlike <bersenev@mail.ru>
2019-12-07 21:50:46 +0300

    Финальное ДЗ№5. Убрал лишний закомментированный код. Теперь точно все!

--- test_main_page.py
+++ test_main_page.py
@@ -2,7 +2,6 @@ from .pages.main_page import MainPage
 from .pages.account_page import AccountPage
 from .pages.basket_page import BasketPage
 from .pages.login_page import LoginPage
-from .pages.product_page import ProductPage
 import pytest
 import time
 
@@ -15,16 +14,6 @@ def test_guest_cant_see_product_in_basket_opened_from_main_page(browser):
     page = BasketPage(browser, browser.current_url)
     page.is_basket_empty()
     page.is_message_basket_empty()
-
-
-# def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
-#     link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
-#     page = ProductPage(browser, link)
-#     page.open()
-#     page.go_to_basket()
-#     page = BasketPage(browser, browser.current_url)
-#     page.is_basket_empty()
-#     page.is_message_basket_empty()
 
 
 @pytest.mark.login_guest
